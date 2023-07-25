c6215b6544202c071fae1c4858566a0a5b850576
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2023-07-25 18:05:11 +0600

    Working on admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,4 +29,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::middleware('admin')->group(function () {
+    Route::get("admin/dashboard", [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+});
+
+require __DIR__ . '/auth.php';
